a7ce3868e585004dc7273a790fc3f9a34fc6efa7
Ej <griimgir@gmail.com>
2021-04-23 20:16:59 -0700

    Updated with AI

--- bot.py
+++ bot.py
@@ -7,6 +7,12 @@ import shutil
 from os import system
 from dotenv import load_dotenv
 import random
+import string
+import json
+from prsaw import RandomStuff #inititate AI repository, Import module DO NOT MESS WITH,. License belongs to https://pypi.org/project/prsaw/
+
+#initiate bank
+os.chdir("bank")
 
 #iniate token
 load_dotenv()
@@ -14,6 +20,7 @@ load_dotenv()
 #initiate prefix
 PREFIX = '='
 gladge = commands.Bot(command_prefix=PREFIX, help_command=None)
+gladgeTalk = RandomStuff(async_mode = True)
 
 #indication that Zhu Li started
 @gladge.event
@@ -26,6 +33,34 @@ async def on_ready():
 async def on_message(message):
     if message.author == gladge.user:
         return
+    
+    if message.content.startswith("=who is your creator"):
+        await message.reply("Ej is my creator")
+    elif message.content.startswith("=who is your creator"):
+        await message.reply("Ej is my creator")
+    elif message.content.startswith("=I am your creator"):
+        await message.reply("No, Ej is my creator")
+    elif message.content.startswith("=i am your creator"):
+        await message.reply("No, Ej is my creator")
+    elif message.content.startswith("=whats your name"):
+        await message.reply("Gladge bot")
+    elif message.content.startswith("=what's your name"):
+        await message.reply("Gladge bot")
+    elif message.content.startswith("=what's your name?"):
+        await message.reply("Gladge bot")
+    elif message.content.startswith("=what is your name"):
+        await message.reply("Gladge bot")
+    elif message.content.startswith("=what is your name?"):
+        await message.reply("Gladge bot")
+    elif message.content.startswith("=who's ej's dj"):
+        await message.reply("jeni is ej's dj")
+    elif message.content.startswith("=who's jeni's dj"):
+        await message.reply("ej")
+    else:
+        if message.content.startswith("="):
+            response = await gladgeTalk.get_ai_response(message.content)
+            await message.reply(response)
+
     await gladge.process_commands(message)    # allows that use of an event before command decorators
 
 @gladge.command(pass_context=True, aliases=['help'])
@@ -64,11 +99,11 @@ async def intro(ctx):
     embedMsg.set_footer(text="Ej™", icon_url="https://cdn.discordapp.com/avatars/211003392347209729/a_562bc1a36d487e3cec4cd56d60d53e84.gif")
 
     embedMsg.add_field(name="Who are these couts that have percieved me", value="  -")
-    embedMsg.add_field(name="Arioli Ravioli", value="aka Ari: https://www.twitch.tv/laziestvillager", inline=False)
+    embedMsg.add_field(name="Arioli Ravioli", value="aka Ari: https://www.twitch.tv/arioli", inline=False)
     embedMsg.add_field(name="Cout 2", value="aka Alexa: https://www.twitch.tv/wwhisper", inline=False)
-    embedMsg.add_field(name="Mom/kararara", value="aka Alex: https://www.twitch.tv/kkaurora", inline=False)
+    embedMsg.add_field(name="Mom/kararara", value="aka Alex: https://www.twitch.tv/kkalexandria", inline=False)
     embedMsg.add_field(name="Devoid", value="aka Devoid: https://www.twitch.tv/devoidxx", inline=False)
-    embedMsg.add_field(name="MoMoo", value="aka momo: https://www.twitch.tv/momobun94", inline=False)
+    embedMsg.add_field(name="MoMoo", value="aka momo: https://www.twitch.tv/momoobun", inline=False)
     embedMsg.add_field(name="----------------------------------------------------------------------", value="```To see my commands type =help```")
 
     await ctx.channel.send(embed=embedMsg)
@@ -80,9 +115,9 @@ async def greetings(ctx):
             responses = ["HI QUEEN, OMG HOW ARE YOU GIRL!", "KAAA WEEEN, WURK IT!", "FLAWLESS, KAA WEEeeEEN!", "Hello cout 2!"]
             choice = random.choice(responses)
             await ctx.channel.send(choice)
-    elif str(ctx.author) in ["Po ⭕#4077"]:
+    elif str(ctx.author) in ["parsecs#0001"]:
             await ctx.channel.send("Greetings, creator.")
-    elif str(ctx.author) in ["laziestvillager#1166"]:
+    elif str(ctx.author) in ["ari 🌿#1166"]:
             await ctx.channel.send("<:pointLaugh:814011766785835008> Hello Arioli Ravioli!")
     else:
         responses = [f"Hello {ctx.author.name}.", f"Hi {ctx.author.name}.", f"Greetings, {ctx.author.name}.", f"Pleasure to see you, {ctx.author.name}.", f"Good day to you, {ctx.author.name}."]
@@ -191,7 +226,7 @@ async def couters(ctx):
     voice = get(gladge.voice_clients, guild=ctx.guild)
     voice.play(discord.FFmpegPCMAudio("soundbytes/cout.mp3"), after=lambda e: end())
     voice.source = discord.PCMVolumeTransformer(voice.source)
-    voice.source.volume = 0.50
+    voice.source.volume = 0.20
 
 @gladge.command(pass_context=True, aliases=['endgame'])
 async def whyers(ctx):
@@ -215,5 +250,40 @@ async def realcout(ctx):
     voice.source = discord.PCMVolumeTransformer(voice.source)
     voice.source.volume = 0.75
 
+@gladge.command(pass_context=True, aliases=['t'])
+async def talk(ctx):
+    if gladge.user == ctx.author:
+        return
+    response = await gladgeTalk.get_ai_response(ctx.content)
+    await ctx.reply(response)
+
 
 gladge.run(os.getenv('TOKEN'))
+
+
+
+
+
+
+
+
+# @gladge.command(pass_context=True, aliases=['bal'])
+# async def balalnce(ctx):
+#     await openAcc(ctx.author)
+
+
+# async def openAcc(user):
+#     with open("bank.json", "r") as f:
+#         users = json.load(f)
+#     if str(user.id) in users:
+#         return False
+#     else:
+#         users[str(user.id)]["wallet"] = 0
+#         users[str(user.id)]["bank"] = 0
+
+#     with open("bank/bank.json", "w") as f:
+#         json.dump(users, f)
+#     return True
+
+# async def get_bank_data():
+#     with open("")
